e0a921ff37cd181eb93615bad35cf98366393256
Andre Ferrer <andrerferrer@hotmail.com>
2019-09-09 18:40:53 -0300

    Add appointments and users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'appointments/index'
+  get 'appointments/show'
+  get 'appointments/new'
+  get 'appointments/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
