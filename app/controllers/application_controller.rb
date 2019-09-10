adc09a38d01017df80888f6df0d3b413cdfcc022
Andre Ferrer <andrerferrer@hotmail.com>
2019-09-10 19:34:44 -0300

    pulled from master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: User::ATTRIBUTES)
+    devise_parameter_sanitizer.permit(:sign_up, keys: User::ATTRIBUTES | Coach::ATTRIBUTES)
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: User::ATTRIBUTES | Coach::ATTRIBUTES)
   end
 end
