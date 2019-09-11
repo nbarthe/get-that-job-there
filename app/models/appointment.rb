a5a92ff6aeee5e82e3a80385590c75593caf8fe8
Andre Ferrer <andrerferrer@hotmail.com>
2019-09-11 12:05:19 -0300

    Add validation to the new appointment form

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,15 @@ class Appointment < ApplicationRecord
   ATTRIBUTES = %i[schedule_at duration user_id coach_id]
   belongs_to :user
   belongs_to :coach
+
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 8 }
+
+  # this is a failed attempt of validating schedule_at
+  # validate :schedule_at_is_valid_datetime
+
+  # private
+
+  # def schedule_at_is_valid_datetime
+  #   errors.add(:happened_at, "We don't have time machines yet") if (schedule_at < Time.zone.now)
+  # end
 end
