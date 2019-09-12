b92d56d12ce71062c51fd7bf3614cc1ceb82bac0
Natalia Barthe <natibarthe29@hotmail.com>
2019-09-12 14:04:05 -0300

    added calendar view and working on appointments view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_204702) do
+ActiveRecord::Schema.define(version: 2019_09_12_144217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_204702) do
     t.integer "coach_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "users", force: :cascade do |t|
