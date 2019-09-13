177cd6cc7ab76f8558dd0e48dd6edbfc8c9c192a
Andre Ferrer <andrerferrer@hotmail.com>
2019-09-13 12:02:59 -0300

    Failed attempt to solve the devise log out bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users do
+
+  resources :users, except: :show do
     resources :appointments, only: [ :index]
   end
+
   resources :coaches, only: [ :index, :show ] do
     resources :appointments, only: [ :index, :new, :create ]
   end
